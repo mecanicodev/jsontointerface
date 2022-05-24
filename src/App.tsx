bf668bb155da8ffbfe44fcd7c8d87b6b55e53f25
Anibal Español <mecanicodev@gmailcom>
2022-05-24 17:07:06 -0400

    Layout improvements

--- src/App.tsx
+++ src/App.tsx
@@ -70,9 +70,9 @@ const App: React.FC = ()=> {
 
   return (
     <Box px={'20px'} className='container' height={{base: '100%', sm: '100vh'}}>
-      <Flex align={'center'} height={'10vh'} >
+      <Flex align={'center'} height={'10vh'} mx={'auto'} maxW={'1200px'}>
         <VscSymbolInterface size='4em'/>
-        <Heading size={'md'}>JSON to Interface App</Heading>
+        <Heading size={'lg'}>JSON to Interface App</Heading>
       </Flex>
       <Box mx={'auto'} maxW={'1200px'}>
         <SimpleGrid
@@ -83,18 +83,18 @@ const App: React.FC = ()=> {
           height={{base: 'auto', sm: '84vh'}}
           >
           <Flex direction={'column'}>
-            <Center className='section-title' height={'4vh'}>
+            <Center className='section-title' height={'4vh'} fontSize='md'>
               Input: JSON format
             </Center>
             <Editor
               loading={
-              <Spinner
-              thickness='6px'
-              speed='0.65s'
-              emptyColor='gray.400'
-              color='blue.500'
-              size='xl'
-              />
+                <Spinner
+                thickness='6px'
+                speed='0.65s'
+                emptyColor='gray.400'
+                color='blue.500'
+                size='xl'
+                />
               }
               height={editorHeight}
               width="100%"
@@ -106,7 +106,7 @@ const App: React.FC = ()=> {
               />
           </Flex>
           <Flex direction={'column'}>
-            <Center className='section-title' height={'4vh'}>
+            <Center className='section-title' height={'4vh'} fontSize='md'>
               Output: Typescript interface
             </Center>
             <Code
@@ -124,7 +124,7 @@ const App: React.FC = ()=> {
         </Progress>
       </Box>
       <Box height={'5vh'}>
-        <Center fontSize={'xs'}>Created by @mecanicodev</Center>
+        <Center fontSize={'xl'}>Created by @mecanicodev</Center>
       </Box>
     </Box>
   );
